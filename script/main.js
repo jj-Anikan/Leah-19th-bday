9564f06de2a5c12de66104bbd5df59c9deaeb8c9
jjmayhew27 <jjmayhew27@gmail.com>
2023-10-13 16:44:52 -0400

    Add a html file and add main commands in main.js

--- script/main.js
+++ script/main.js
@@ -6,3 +6,23 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspecitveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRederer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+const mazeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const mazeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const mazeCube = new THREE.Mesh(mazeGeometry, mazeMaterial);
+scene.add(mazeCube);
+
+// Set the camera position
+camera.position.z = 5;
+
+// Game loop
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    // Add game logic here (e.g., player movement)
+
+    renderer.render(scene, camera);
+};
+
+// Start the game loop
+animate();
